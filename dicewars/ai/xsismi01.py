e0775828d4fd137908a19eabd772f12a2ae5d800
Lukáš Šišmiš <xsismi01@stud.fit.vutbr.cz>
2020-01-03 21:31:59 +0100

    AI added

--- dicewars/ai/xsismi01.py
+++ dicewars/ai/xsismi01.py
@@ -1,6 +1,6 @@
 import logging
 from dicewars.ai.utils import possible_attacks
-from dicewars.ai.utils import probability_of_holding_area, probability_of_successful_attack
+from dicewars.ai.utils import probability_of_holding_area, probability_of_successful_attack, attack_succcess_probability
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
 import copy
@@ -52,6 +52,21 @@ class AI:
         newBoard.get_area(sourceName).set_dice(1)
         return newBoard
 
+    def updateBoardAttack(self, board, source, target):
+        sourceName = source.get_name()
+        targetName = target.get_name()
+        newBoard = copy.deepcopy(board)
+        newBoard.get_area(targetName).set_owner(sourceName)
+        newBoard.get_area(targetName).set_dice(source.get_dice() - 1)
+        newBoard.get_area(sourceName).set_dice(1)
+        return newBoard
+
+    def updateBoardDefence(self, board, defender, attacker):
+        targetName = attacker.get_name()
+        newBoard = copy.deepcopy(board)
+        newBoard.get_area(targetName).set_dice(1)
+        return newBoard
+
     def best_turn(self):
         """Get a list of preferred moves
 
@@ -68,32 +83,75 @@ class AI:
             hold_prob = atk_prob * probability_of_holding_area(self.board, target.get_name(), atk_power - 1, self.player_name)
             if hold_prob >= 0.2 or atk_power == 8:
                 newBoard = self.updateBoard(self.board, source, target)
-                turns.append([area_name, target.get_name(), self.expectiMinMax(newBoard, target, False)])
+                turns.append([area_name, target.get_name(), self.expectiMax(newBoard, target)])
 
         return sorted(turns, key=lambda turn: turn[2], reverse=True)
 
-    def expectiMinMax(self, board, area, min_or_max):
-        # min_or_max - FALSE - min; TRUE - max
-        turns = []
-        for source, target in self.possible_attacks_from_area(board, area, self.player_name):
-            area_name = source.get_name()
+    def expectiMax(self, board, area):
+        values=[]
+        if area.get_dice() < 2:
+            return 0.0
+
+        for source, target in self.possible_attacks_from_area(board, area, self.player_name): 
             atk_power = source.get_dice()
             if atk_power < 2:
                 continue
-            atk_prob = probability_of_successful_attack(board, area_name, target.get_name())
-            hold_prob = atk_prob * probability_of_holding_area(board, target.get_name(), atk_power - 1, self.player_name)
-            if hold_prob >= 0.2 or atk_power == 8:
-                if min_or_max:
-                    turns.append(self.expectiMinMax(self.updateBoard(board, source, target), target, False))
-                else:
-                    turns.append(self.expectiMinMax(self.updateBoard(board, source, target), target, True))
-            else:
-                turns.append(hold_prob)
 
-        if not turns:
+            attack_possibility_value = attack_succcess_probability(source.get_dice(), target.get_dice())
+            if attack_possibility_value >= 0.2 or atk_power == 8:
+                newBoard = self.updateBoardAttack(board, source, target) # ze sme zautocili a vyhrali
+                areaAttacked = newBoard.get_area(target.get_name()) # areaAttacked bude mat get_dice() - 1 kociek
+                val = attack_possibility_value * self.expectiMin(newBoard, areaAttacked) * areaAttacked.get_dice() 
+                values.append(val)
+
+        if not values:
             return 0.0
+        
+        return max(values)
 
-        return max(turns) if min_or_max else min(turns)
+    def expectiMin(self, board, area):
+        values = []
+        for adj in area.get_adjacent_areas():
+            adjacent_area = board.get_area(adj)
+            if adjacent_area.get_owner_name() != self.player_name:
+                enemy_dice = adjacent_area.get_dice()
+                if enemy_dice == 1:
+                    continue
+                lose_prob = attack_succcess_probability(enemy_dice, area.get_dice())
+                if lose_prob >= 0.2 or area.get_dice() == 8:        
+                    newBoard = self.updateBoardDefence(board, area, adjacent_area) # ze na nas zautocili a vyhrali sme a s area nic nestane, oni budu mat get_dice() == 1
+                    areaThatAttacked = newBoard.get_area(adjacent_area.get_name()) # areaAttacked bude mat get_dice() == 1 kocku
+                    val = (1 - lose_prob) * self.expectiMax(newBoard, areaThatAttacked) * areaThatAttacked.get_dice() 
+                    values.append(val)
+
+        if not values:
+            return 1.0 # tu by som mozno dal jednotku
+        
+        return min(values)
+
+
+    # def expectiMinMax(self, board, area, min_or_max):
+    #     # min_or_max - FALSE - min; TRUE - max
+    #     turns = []
+    #     for source, target in self.possible_attacks_from_area(board, area, self.player_name):
+    #         area_name = source.get_name()
+    #         atk_power = source.get_dice()
+    #         if atk_power < 2:
+    #             continue
+    #         atk_prob = probability_of_successful_attack(board, area_name, target.get_name())
+    #         hold_prob = atk_prob * probability_of_holding_area(board, target.get_name(), atk_power - 1, self.player_name)
+    #         if hold_prob >= 0.2 or atk_power == 8:
+    #             if min_or_max:
+    #                 turns.append(self.expectiMinMax(self.updateBoard(board, source, target), target, False))
+    #             else:
+    #                 turns.append(self.expectiMinMax(self.updateBoard(board, source, target), target, True))
+    #         else:
+    #             turns.append(hold_prob)
+
+    #     if not turns:
+    #         return 0.0
+
+    #     return max(turns) if min_or_max else min(turns)
 
     def possible_attacks_from_area(self, board, area, player_name): # -> Iterator[Tuple[int, int]] # : Board, area: Area, player_name: int
         neighbours = area.get_adjacent_areas()
