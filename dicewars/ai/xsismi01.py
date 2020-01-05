7911aeac0c362b1af2eb9d375b8bb25b06296f81
Adam Masaryk <masarykadam@gmail.com>
2020-01-05 02:50:59 +0100

    Reworked AI

--- dicewars/ai/xsismi01.py
+++ dicewars/ai/xsismi01.py
@@ -5,6 +5,8 @@ from dicewars.ai.utils import probability_of_holding_area, probability_of_succes
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
 import copy
 
+THRESHOLD = 0.2
+
 class AI:
     """
     Agent using Expectiminimax strategy
@@ -25,7 +27,7 @@ class AI:
         self.board = board
         self.playersCount = self.board.nb_players_alive()
 
-        best_turn = self.best_turn()
+        best_turn = self.expectiMinMax()
 
         if best_turn[0] is not None:
             return BattleCommand(best_turn[0], best_turn[1])
@@ -47,68 +49,54 @@ class AI:
         newBoard.get_area(targetName).set_dice(1)
         return newBoard
 
-    def best_turn(self):
-        best_turn = (None, None, -1.0)
+    def expectiMinMax(self):
+        best_turn = (None, None, 0.0)
 
-        for source, target in possible_attacks(self.board, self.player_name):
-            area_name = source.get_name()
-            atk_power = source.get_dice()
-            atk_prob = probability_of_successful_attack(self.board, area_name, target.get_name())
-            hold_prob = atk_prob * probability_of_holding_area(self.board, target.get_name(), atk_power - 1, self.player_name)
-            if hold_prob >= 0.2 or atk_power == 8:
-                newBoard = self.updateBoardAttack(self.board, source, target)
-                new_value = self.expectiMax(newBoard, target)
-                if new_value > best_turn[2]:
-                    best_turn = (area_name, target.get_name(), new_value)
+        for area in self.board.get_player_border(self.player_name):
+            exp = self.expectiMax(self.board, area)
+            if exp[1] > best_turn[2]:
+                best_turn = (area.get_name(), exp[0], exp[1])
 
         return best_turn
 
     def expectiMax(self, board, area):
-        if area.get_dice() < 2:
-            return 0.0
+        best_move = (None, 0.0)
+        atk_power = area.get_dice()
+        if not area.can_attack():
+            return best_move
 
-        values = []
-
-        for source, target in self.possible_attacks_from_area(board, area, self.player_name): 
-            atk_power = source.get_dice()
-
-            attack_possibility_value = attack_succcess_probability(source.get_dice(), target.get_dice())
-            if attack_possibility_value >= 0.2 or atk_power == 8:
-                newBoard = self.updateBoardAttack(board, source, target) # ze sme zautocili a vyhrali
+        for target in self.possible_attacks_from_area(board, area, self.player_name): 
+            attack_possibility_value = attack_succcess_probability(atk_power, target.get_dice())
+            if attack_possibility_value >= THRESHOLD or atk_power == 8:
+                newBoard = self.updateBoardAttack(board, area, target) # ze sme zautocili a vyhrali
                 areaAttacked = newBoard.get_area(target.get_name()) # areaAttacked bude mat get_dice() - 1 kociek
                 val = attack_possibility_value * self.expectiMin(newBoard, areaAttacked, self.playersCount) * areaAttacked.get_dice() 
-                values.append(val)
-
-        if not values:
-            return 0.0
+                if val > best_move[1]:
+                    best_move = (target.get_name(), val)
         
-        return max(values)
+        return best_move
 
     def expectiMin(self, board, area, expectiMinLayers):
         values = []
-        for adj in area.get_adjacent_areas():
-            adjacent_area = board.get_area(adj)
-            if adjacent_area.get_owner_name() != self.player_name:
-                enemy_dice = adjacent_area.get_dice()
-                if enemy_dice == 1:
-                    continue
-                lose_prob = attack_succcess_probability(enemy_dice, area.get_dice())
-                if lose_prob <= 0.25 or area.get_dice() == 8:        # tu ma byt snad lose_prob <= 0.2 nieeeee? ze je to 80% sanca ze oni prehraju
-                    newBoard = self.updateBoardDefence(board, area, adjacent_area) # ze na nas zautocili a vyhrali sme a s area nic nestane, oni budu mat get_dice() == 1
-                    areaThatAttacked = newBoard.get_area(adjacent_area.get_name()) # areaAttacked bude mat get_dice() == 1 kocku
-                    expectiVal = 0
-                    if expectiMinLayers == 0:
-                        expectiVal = self.expectiMax(newBoard, areaThatAttacked) 
-                    else: 
-                        expectiVal = self.expectiMin(newBoard, areaThatAttacked, expectiMinLayers - 1)
+        area_dice = area.get_dice()
 
-                    val = (1 - lose_prob) * expectiVal * areaThatAttacked.get_dice() 
-                    values.append(val)
+        for adjacent_area in self.possible_attacks_from_area(board, area, self.player_name):
+            enemy_dice = adjacent_area.get_dice()
+            if not adjacent_area.can_attack():
+                continue
+            lose_prob = attack_succcess_probability(enemy_dice, area_dice)
+            if lose_prob <= THRESHOLD or area_dice == 8:        # tu ma byt snad lose_prob <= 0.2 nieeeee? ze je to 80% sanca ze oni prehraju
+                newBoard = self.updateBoardDefence(board, area, adjacent_area) # ze na nas zautocili a vyhrali sme a s area nic nestane, oni budu mat get_dice() == 1
+                areaThatAttacked = newBoard.get_area(adjacent_area.get_name()) # areaAttacked bude mat get_dice() == 1 kocku
+                if expectiMinLayers == 0:
+                    expectiVal = self.expectiMax(newBoard, areaThatAttacked)[1]
+                else: 
+                    expectiVal = self.expectiMin(newBoard, areaThatAttacked, expectiMinLayers - 1)
 
-        if not values:
-            return 1.0
+                val = (1 - lose_prob) * expectiVal * areaThatAttacked.get_dice() 
+                values.append(val)
         
-        return min(values)
+        return min(values) if values else 1.0
 
     def possible_attacks_from_area(self, board, area, player_name):
         neighbours = area.get_adjacent_areas()
@@ -116,4 +104,4 @@ class AI:
         for adj in neighbours:
             adjacent_area = board.get_area(adj)
             if adjacent_area.get_owner_name() != player_name:
-                yield area, adjacent_area
+                yield adjacent_area
